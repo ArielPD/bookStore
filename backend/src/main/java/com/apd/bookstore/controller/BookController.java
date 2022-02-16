ee4f184b1657a8835f8b90be4482df86a191e8f8
Ariel Pinto <arielp@goja.com>
2022-02-16 14:36:41 -0400

    add prod config and add swagger

--- backend/src/main/java/com/apd/bookstore/controller/BookController.java
+++ backend/src/main/java/com/apd/bookstore/controller/BookController.java
@@ -2,6 +2,10 @@ package com.apd.bookstore.controller;
 
 import com.apd.bookstore.dto.BookDto;
 import com.apd.bookstore.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+@Api(value = "Book Api", tags = "Book Api", produces = "application/json")
 @RestController
 @RequestMapping("api/v1/books")
 public class BookController {
@@ -19,6 +24,12 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @ApiOperation(value = "get list of books", response = BookDto[].class, produces = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list of book"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "not found resource")
+    })
     @GetMapping
     public ResponseEntity<List<BookDto>> getBooks(){
         List<BookDto> books = bookService.getBooks();
--- backend/src/main/java/com/apd/bookstore/dto/BookDto.java
+++ backend/src/main/java/com/apd/bookstore/dto/BookDto.java
@@ -1,5 +1,7 @@
 package com.apd.bookstore.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,11 +13,44 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "BookDto", description = "All detail about book")
 public class BookDto {
 
+    @ApiModelProperty(
+            readOnly = true,
+            value = "UUID",
+            dataType = "UUID",
+            example = "047aaecc-8e4d-11ec-b909-0242ac120099",
+            notes= "The database generated uuid for book id",
+            required = true
+    )
     private UUID id;
+
+    @ApiModelProperty(
+            value = "title",
+            dataType = "String",
+            example = "book title",
+            notes= "Book title",
+            required = true
+    )
     private String title;
+
+    @ApiModelProperty(
+            value = "description",
+            dataType = "String",
+            example = "book description",
+            notes= "Book description",
+            required = true
+    )
     private String description;
+
+    @ApiModelProperty(
+            value = "releaseYear",
+            dataType = "int",
+            example = "2020",
+            notes= "Book releaseYear",
+            required = true
+    )
     private int releaseYear;
 
 }
