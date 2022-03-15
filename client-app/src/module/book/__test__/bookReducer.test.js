3b2ff845e7d71c8c2f5b293c11d893b0640e5280
Ariel Pinto <arielp@goja.com>
2022-03-15 12:09:32 -0400

    Add reducer for filterBookByTitle and add unit test in UI react

--- client-app/src/module/book/__test__/bookReducer.test.js
+++ client-app/src/module/book/__test__/bookReducer.test.js
@@ -27,5 +27,32 @@ describe('bookReducer', () => {
             }    
         })
     })
+
+    it('should return right new state for BOOKBYTITLE Action', () => {
+        const action = {
+            type: 'BOOKSBYTITLE',
+            payload: [{
+                id: 1,
+                title: 'second book',
+                description: 'des',
+                releaseYear: 2022
+            },
+            ],
+        };
+
+        const newState = bookReducer(INITIAL_BOOK_REDUCER_STATE, action);
+
+        expect(newState).toEqual({
+            books: [{
+                id: 1,
+                title: 'second book',
+                description: 'des',
+                releaseYear: 2022
+            }],
+            promise: {
+                isPending: false, isFulfilled: false, isErrorOcurred: false
+            },    
+        })
+    })
 })
 
